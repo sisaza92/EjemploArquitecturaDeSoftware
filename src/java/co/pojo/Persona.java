5e9bbe64e18e1fe7edd0f8ba2859a6475c7fbd43
ccamilo.isaza <ccamilo.isaza@localhost>
2015-02-09 14:49:22 -0500

    Version Inicial

---
+++ src/java/co/pojo/Persona.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.pojo;
+
+/**
+ *
+ * @author ccamilo.isaza
+ */
+public class Persona {
+    
+    private String nombres;
+    private String apellidos;
+    private int edad;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+}
